38dfad509a57c91ce1bc1bbd09e10029407dc870
Imtiyaz Ahmad <imtiyaz@afeela.onmicrosoft.com>
2025-09-25 22:41:40 +0530

    add pipeline

--- src/obj/Debug/net8.0/BookLendingApp.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/BookLendingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookLendingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0fbdabdac5c3b03b5e6b786f51217366e183365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d7e7ca6cd0510633166b5a76ef1281bced1f059")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookLendingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookLendingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
